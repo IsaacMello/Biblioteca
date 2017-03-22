9b6d980619c0dc63cb37116d3b85a444bb491784
IsaacMello <ifms@ic.ufal.br>
2017-03-22 13:27:46 -0300

    Update UsuariosExistentes.java

--- src/Usuario/UsuariosExistentes.java
+++ src/Usuario/UsuariosExistentes.java
@@ -5,7 +5,7 @@ public class UsuariosExistentes {
 	String nome, matricula;
 
 	public String nome() {
-		this.nome = "Isaac Fernando Mello Silva";
+		this.nome = "Fulano de Tal";
 
 		return nome;
 	}
